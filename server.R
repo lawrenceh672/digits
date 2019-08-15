c36cef64eaa96b3c07abb12c670c88394e6b6ab7
lawrence <lawrenceh672@gmail.com>
2019-08-15 09:33:34 -0500

    code cleaning

--- server.R
+++ server.R
@@ -12,17 +12,30 @@ library(graphics)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
-  data_loaded <- 0
-  sample_size <- 42000  #default 42000 all the training samples
-  rect(0,0,10,10, border="red", col = "blue") #blue is not loaded
-  text(0,0, "Not Loaded")
+  data_loaded <<- 0 #not loaded it by default
+  sample_size <<- 20  #default 20 all the training samples
   
+  #the sample size reactor
+  sample_size_reactor<-reactive({
+    sample_size<-input$samples**2 #sqaure it for square pictures
+    sample_size
+  })
+    
   output$numbers <- renderPlot({
+  if(data_loaded == FALSE){
+    rect(0,0,100,100, border="red", col = "blue") #blue is not loaded
+    text(0,0, "Not Loaded")
+    data_loaded<-TRUE
+    }
+  
+  if(data_loaded == TRUE) {
+      rect(0,0,100,100, border="blue", col = "red") #blue is not loaded
+      text(0,0, "Loaded")
+      data_loaded=2
+    }
   })
   
-  observeEvent(input$load_button, {
-    #draw a green box
-    rect(0,0,100,100, border="green", col = "green")
-    data_loaded <- 1 #now its true, presumably.
+  output$samples <- renderText({
+    sample_size_reactor()
   })
 })
--- ui.R
+++ ui.R
@@ -3,23 +3,22 @@
 library(shiny)
 
 
-shinyUI(fluidPage(
-  titlePanel("digits"),
+shinyUI(
+  fluidPage(
+    titlePanel("digits"),
   
-  # Sidebar with a slider input for number of sample, all sqaured
-  sidebarLayout(
-    sidebarPanel(
-      actionButton("load_button", "Load sample Data"),
-      sliderInput("samples",
-                   "Number of samples:",
-                   min = 1,
-                   max = 10,
-                   value = 3)
-    ),
+    # Sidebar with a slider input for number of sample, all sqaured
+    sidebarLayout(
+      sidebarPanel(
+        h3("Load the data set"),
+        submitButton("Load Data"),
+        h3("Number of samples to start with"),
+        sliderInput("samples","Number of samples:",min = 1,max = 10,value = 3)
+        ),
     
     #Draw the
     mainPanel(
-       plotOutput("numbers")
-    )
-  )
-))
+      plotOutput("numbers"),
+      h3("sample Size"),
+      textOutput("samples")
+))))
