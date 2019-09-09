175cdc0cb5de7a52cdebd074002d6f40abaad342
Larry <lawrenceh672@gmail.com>
2019-09-09 11:58:59 -0500

    Update server.R

--- server.R
+++ server.R
@@ -11,31 +11,11 @@ library(shiny)
 library(graphics)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output, session) {
+shinyServer(function(input, output) {
   data_loaded <<- 0 #not loaded it by default
   sample_size <<- 20  #default 20 all the training samples
   
-  #the sample size reactor
-  sample_size_reactor<-reactive({
-    sample_size<-input$samples**2 #sqaure it for square pictures
-    sample_size
-  })
-    
-  output$numbers <- renderPlot({
-  if(data_loaded == FALSE){
-    rect(0,0,100,100, border="red", col = "blue") #blue is not loaded
-    text(0,0, "Not Loaded")
-    data_loaded<-TRUE
-    }
-  
-  if(data_loaded == TRUE) {
-      rect(0,0,100,100, border="blue", col = "red") #blue is not loaded
-      text(0,0, "Loaded")
-      data_loaded=2
-    }
-  })
-  
-  output$samples <- renderText({
-    sample_size_reactor()
-  })
+  train<<-0
+  t2<<-0
+  t1<<-0
 })
