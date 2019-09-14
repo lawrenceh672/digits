ce6b377dfe15bcddef0632ef0b2c64fb16140f29
Larry <lawrenceh672@gmail.com>
2019-09-14 09:02:40 -0500

    Update svm.r

--- svm.r
+++ svm.r
@@ -15,9 +15,11 @@ rows <- seq_len(nrow(train))
 
 training_rows <- sample(rows, size = number_training_samples)
 #remove training rows from rows
-rows <- training_rows[! rows %in% training_rows]
+rows<-rows[-training_rows]
 #select the testing rows
 testing_rows <- sample(rows, size = number_testing_samples)
+#remove the testing rows
+rows<-rows[-testing_rows]
 
 training <- train[training_rows, ] #training data set
 testing <- train[testing_rows, ] #testing data 
@@ -60,8 +62,8 @@ svm_model <- parallelSVM(label ~ ., training,scale=FALSE,method = "C-classificat
 svm_prediction <- predict(svm_model, newdata=testing[,-1])
 
 #Create a confusion matrix
-xtab2 <- table(svm_prediction,testing$label)
-xtab2
+confusion_matrix <- table(svm_prediction,testing$label)
+confusion_matrix
 
 #add a column for the svm prediction to the testing data 
 train2_class <- cbind(testing,svm_prediction)
@@ -74,18 +76,28 @@ png("digit_1024.png", units="px", width=4800, height=4800, res=32)
 par(mfrow=c(32,32))
 
 for(r in 1:nrow(testing)){
+  #skip the first column which has the label
   c <- 2
   plot(1,1,asp = 1,pch=20,cex=0,xlim=c(1,28),ylim=c(1,28),xaxt="n",yaxt="n",xlab="",ylab="",bty ="n",axes=F,frame.plot=F)
   for(i in 1:28){
     for(j in 1:28){
-      training_image_matrix[i,j] <- testing[r,c]
-      points(i,j,pch=15,cex=3,col=add.alpha(color_names[as.numeric(testing[r,1])],alpha=(training_image_matrix[i,j]/255)),xaxt="n",yaxt="n",xlab="",ylab="",bty ="n",axes=F,frame.plot=F)
-      c <- c+1
+      #fill in each pixel
+      pixel_value<-testing[r,c]#pixel value
+      training_image_matrix[i,j] <- pixel_value
+      #color for the number from the color names index
+      color <- color_names[as.numeric(testing[r,1])]
+      points(i,j,pch=15,
+             cex=3,
+             col=add.alpha(color,alpha=(pixel_value/255)),
+             xaxt="n",yaxt="n",
+             xlab="",ylab="",
+             bty ="n",axes=F,
+             frame.plot=F)
+      c <- c+1 #advance to next pixel
     }
   }
 }
 dev.off ()
-#
 
 training_image_matrix <- data.frame(matrix(0,nrow=28,ncol=28))
 
